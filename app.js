dfa07d4c778299644df467978751e0db0e23a755
Lioba <ldonoso@dokify.net>
2014-12-02 23:12:06 +0100

    new app

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
 var express = require('express');
 var app 	= express();
-var fs 		= require('fs');
-var routes	= fs.readdirSync('./routes');
 
-console.log('He cargado la ruta ' + routes);
-
-var server = app.listen(8080, function () {
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-});
-
-console.log('1');
-
-// app.get('/', function(){
-// 	console.log('routaok');
-// });
 module.exports = app;
